15f35ec613469ca620b8aeee91c7b8908fe8d1f3
s242825 <s242825@dtu.dk>
2025-11-23 16:15:42 +0100

    Added hcp run script

---
+++ job.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J train_transformer
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 1:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=15GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s242825@dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
+
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
+
+# Check if CUDA is working
+/appl/cuda/11.6.0/samples/bin/x86_64/linux/release/deviceQuery
+
+# Load python
+module load python3/3.12.9
+
+# Activate venv for training
+source transformer/bin/activate
+
+# Run training script
+python3.12 main.py
